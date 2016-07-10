dd9f4e6ec9fcb55e5f65da80c62cef5fb9c0d4f3
Felix <felixbruell@live.de>
2016-07-10 17:58:05 +0200

    added more functionality to Color class (operator overload etc.)

--- Source/Framework/Color.h
+++ Source/Framework/Color.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../Utility/Tools.h"
 
 class Color
 {
@@ -13,6 +14,79 @@ public:
 	Color()
 		: Color(0.0f, 0.0f, 0.0f, 1.0f)
 	{}
+
+	// operators
+	Color& operator+=(const Color& rhs)
+	{
+		r += rhs.r;
+		g += rhs.g;
+		b += rhs.b;
+		a += rhs.a;
+		normalize();
+		return *this;
+	}
+	Color& operator+(const Color& rhs) const
+	{
+		Color c(*this);
+		return c += rhs;
+	}
+	Color& operator*=(const Color& rhs)
+	{
+		r *= rhs.r;
+		g *= rhs.g;
+		b *= rhs.b;
+		a *= rhs.a;
+		normalize();
+		return *this;
+	}
+	Color& operator*(const Color& rhs) const
+	{
+		Color c(*this);
+		return c *= rhs;
+	}
+	Color& operator*=(float scalar)
+	{
+		r *= scalar;
+		g *= scalar;
+		b *= scalar;
+		a *= scalar;
+		normalize();
+		return *this;
+	}
+	Color& operator*(float scalar) const
+	{
+		Color c(*this);
+		return c *= scalar;
+	}
+
+	// additional functionality
+
+	// red + green + blue
+	float getColorSum() const
+	{
+		return r + g + b;
+	}
+
+	// indicates how bright the color is for the human eye
+	float getLuminance() const
+	{
+		return (*this * getLuminanceConst()).getColorSum();
+	}
+
+	static Color mix(Color c1, Color c2, float fac)
+	{
+		return tool::mix(c1, c2, fac);
+	}
+
+	Color& normalize()
+	{
+		r = tool::clamp(r, 0.0f, 1.0f);
+		g = tool::clamp(g, 0.0f, 1.0f);
+		b = tool::clamp(b, 0.0f, 1.0f);
+		a = tool::clamp(a, 0.0f, 1.0f);
+
+		return *this;
+	}
 
 	// predefined colors
 	static Color Red()
@@ -68,6 +142,10 @@ public:
 		return Color(0.2f, 0.2f, 0.2f);
 	}
 
+	static Color getLuminanceConst()
+	{
+		return Color(0.299f, 0.587f, 0.114f, 0.0f);
+	}
 public:
 	float r, g, b, a;
 };
