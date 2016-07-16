4ae9559d4f2e9f44083b1f132e86dc50019e98c4
Felix Brüll <felixbruell@live.de>
2016-07-16 20:56:11 +0200

    completed font class (not tested)

---
+++ Source/Framework/OpenGL/Font.cpp
@@ -0,0 +1,236 @@
+#include "Font.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+Font::Font()
+{
+	m_pMetrics = std::unique_ptr<FT_Glyph_Metrics[]>(new FT_Glyph_Metrics[m_nChars]);
+	memset(m_pMetrics.get(), 0, sizeof(FT_Glyph_Metrics) * m_nChars);
+}
+
+void Font::load(FT_LibraryRec_ * ft, const std::string & filename, float scalar)
+{
+	m_ft = ft;
+	long filesize = 0;
+
+	// read data from file
+	{
+		FILE* pFile = fopen(filename.c_str(), "rb");
+		if (!pFile)
+			throw Exception("missing file: " + filename);
+
+		// obtain file size:
+		fseek(pFile, 0, SEEK_END);
+		filesize = ftell(pFile);
+		rewind(pFile);
+
+		m_rawData = std::unique_ptr<char[]>(new char[filesize]);
+
+		auto count = fread(m_rawData.get(), 1, filesize, pFile);
+		fclose(pFile);
+		
+		if (count != filesize)
+			throw Exception("could not read all bytes from: " + filename);
+	}
+
+	// create new memory face
+	auto fterr = FT_New_Memory_Face(m_ft, 
+		reinterpret_cast<FT_Byte*>(m_rawData.get()), filesize,
+		0, &m_face);
+	if (fterr)
+		throw Exception("invalid font file: " + filename);
+
+	// TODO think about it
+	// setting texture size int pixel
+	fterr = FT_Set_Pixel_Sizes(m_face, 0, int(scalar * 1.5f));
+
+	findMaxBearing(scalar);
+
+	// Shader
+	loadShader("data/Shader/text");
+}
+
+void Font::create()
+{
+	Shader::create();
+
+	// set uniform default values
+	glUseProgram(m_program);
+	glUniform1i(m_uniLocTexture, 0);
+	glUniform4f(m_uniLocColor, 1.0f, 1.0f, 1.0f, 1.0f);
+	glUseProgram(0);
+
+	glCheck("Font::create uniforms");
+
+	loadTextures();
+
+	glCheck("Font::create textures");
+}
+
+void Font::dispose()
+{
+	glDeleteTextures(CHAR_END - CHAR_START, m_textureArray);
+	memset(m_textureArray, 0, sizeof(m_textureArray));
+
+	Shader::dispose();
+}
+
+void Font::write(const std::string& txt, PointF pos) const
+{
+	// bind shader
+	bind();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	// Loop through all chars
+	for (const auto& c : txt)
+	{
+		if (c < CHAR_START || c > CHAR_END)
+			continue;
+
+		const FT_Glyph_Metrics& m = m_pMetrics[c];
+
+		// calculate position things
+		float bmpHeight = m.height * m_scaleFactor;
+		float offHeight = (m_maxBearing - m.horiBearingY) * m_scaleFactor;
+
+		float bmpWidth = m.width * m_scaleFactor;
+		float advance = m.horiAdvance * m_scaleFactor;
+		float bearX = m.horiBearingX * m_scaleFactor;
+
+		// bind and draw texture
+		assert(m_textureArray[c - CHAR_START]);
+		glBindTexture(GL_TEXTURE_2D, m_textureArray[c - CHAR_START]);
+
+		glCheck("font::write before glBegin");
+		glBegin(GL_TRIANGLE_STRIP);
+		{
+			glVertex4f(pos.x + bearX, pos.y + offHeight, 0, 0);
+			glVertex4f(pos.x + bearX + bmpWidth, pos.y + offHeight, 1, 0);
+			glVertex4f(pos.x + bearX, pos.y + offHeight + bmpHeight, 0, 1);
+			glVertex4f(pos.x + bearX + bmpWidth, pos.y + offHeight + bmpHeight, 1, 1);
+		}
+		glEndSafe();
+
+		// Advance the cursor to the start of the next character
+		pos.x += advance;
+	}
+
+	glDisable(GL_BLEND);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	unbind();
+	glCheck("font::write after drawing");
+}
+
+PointF Font::getDim(const char* txt) const
+{
+	float width = 0.0f;
+	while(*txt != '\0')
+	{
+		assert(*txt < m_nChars);
+		if(*txt < m_nChars)
+			width += m_pMetrics[*txt].horiAdvance;
+		txt++;
+	}
+	// return actual width + max font height (is better for positioning etc.)
+	return PointF(width * m_scaleFactor, m_fontHeight);
+}
+
+PointF Font::getDim(const std::string& txt) const
+{
+	return getDim(txt.c_str());
+}
+
+void Font::setColor(const Color& c)
+{
+	glUseProgram(m_program);
+	glUniform4f(m_uniLocColor, c.r, c.g, c.b, c.a);
+	glUseProgram(0);
+}
+
+void Font::loadUniforms()
+{
+	// locate unitforms
+	m_uniLocTexture = glGetUniformLocation(m_program, "tex");
+	if (m_uniLocTexture == -1)
+		throw Exception("font shader: tex uniform not found");
+
+	m_uniLocColor = glGetUniformLocation(m_program, "color");
+	if (m_uniLocColor == -1)
+		throw Exception("font shader: tex uniform not found");
+}
+
+void Font::load()
+{
+	// dummy function dont call
+	throw Exception("stupid, don't call this on font...");
+}
+
+void Font::findMaxBearing(float scalar)
+{
+	m_fontHeight = scalar;
+
+	// find max character height to determine texture stretch factor
+
+	m_maxBearing = 0;
+	int maxDown = 0;
+	for(int i = CHAR_START; i <= CHAR_END; i++)
+	{
+		if(FT_Load_Char(m_face,i,FT_LOAD_RENDER))
+			continue;
+
+		// get Metrics
+		m_pMetrics[i] = m_face->glyph->metrics;
+		m_pMetrics[i].horiAdvance = m_face->glyph->advance.x;
+		m_maxBearing = std::max(m_maxBearing, int(m_pMetrics[i].horiBearingY));
+		maxDown = std::max(maxDown, int(m_pMetrics[i].height - m_pMetrics[i].horiBearingY));
+	}
+
+	m_maxHeight = m_maxBearing + maxDown;
+	m_scaleFactor = scalar / m_maxHeight;
+}
+
+void Font::loadTextures()
+{
+	memset(m_textureArray, 0, sizeof(m_textureArray));
+	glGenTextures(CHAR_END - CHAR_START, m_textureArray);
+
+	// glyph : info for out character
+	auto g = m_face->glyph;
+
+	for(int i = CHAR_START; i <= CHAR_END; i++)
+	{
+		int index = i - CHAR_START;
+
+		if (FT_Load_Char(m_face, i, FT_LOAD_RENDER))
+		{
+			// could not load char
+			m_textureArray[index] = 0;
+			continue;
+		}
+
+		glBindTexture(GL_TEXTURE_2D, m_textureArray[index]);
+
+		// We require 1 byte alignment when uploading texture data
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+		// Clamping to edges is important to prevent artifacts when scaling
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		// Linear filtering usually looks best for text 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, g->bitmap.width, g->bitmap.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, g->bitmap.buffer);
+	}
+}
---
+++ Source/Framework/OpenGL/Font.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Shader/Shader.h"
+#include "../../Utility/Point.h"
+#include "../Color.h"
+#include <memory>
+
+class Font : public Shader
+{
+public:
+	Font();
+	virtual ~Font() {}
+
+	void load(struct FT_LibraryRec_* ft, const std::string& filename, float scalar);
+	virtual void create() override;
+	virtual void dispose() override;
+
+	void write(const std::string& txt, PointF pos) const;
+	PointF getDim(const char* txt) const;
+	PointF getDim(const std::string& txt) const;
+	void setColor(const Color& c);
+protected:
+	virtual void loadUniforms() override;
+	void load() override;
+private:
+	void findMaxBearing(float scalar);
+	void loadTextures();
+private:
+	static const int CHAR_START = 32;
+	static const int CHAR_END = 126;
+
+	struct FT_LibraryRec_*	m_ft = nullptr;
+	struct FT_FaceRec_*		m_face = nullptr;
+
+	GLint	m_uniLocColor = -1;
+	GLint	m_uniLocTexture = -1;
+
+	std::unique_ptr<char[]> m_rawData;
+	GLuint	m_textureArray[CHAR_END - CHAR_START];
+
+	static const int m_nChars = 128;
+	std::unique_ptr<struct FT_Glyph_Metrics_[]> m_pMetrics;
+
+	float m_scaleFactor = 0.0f;
+	int m_maxBearing = 0;
+	int m_maxHeight = 0;
+
+	float m_fontHeight = 0.0f; // max fontheigh on display
+};
