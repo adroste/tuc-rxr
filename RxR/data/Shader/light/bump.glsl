1ef71f63e3384109841cd2e27b3a67ea4cd22a50
Felix Brüll <felixbruell@live.de>
2016-07-22 21:12:55 +0200

    added bump mapping helper file (not tested)

---
+++ RxR/data/Shader/light/bump.glsl
@@ -0,0 +1,18 @@
+// bump mapping helper functions
+
+// VertexShader: calculates matrix for bumpReadNormal
+mat3 bumpGetRotation(vec3 inNormal, vec3 inTangent)
+{
+	// reorder tangent so that its perfectly othogonal (improves visual quality)
+	vec3 tangent = normalize(inTangent - dot(inTangent, inNormal) * inNormal);
+
+	vec3 biTangent = cross(tangent, inNormal); // 3rd axis for coordinate system
+	
+	return mat3(tangent, biTangent, inNormal);
+}
+
+// FragmentShader: transfers normal from sampler to correct normal
+vec3 bumpReadNormal(vec3 rawNormal, mat3 matrix)
+{
+	return normalize(matrix * ((255.0 / 128.0 * rawNormal) - 1.0));
+}
