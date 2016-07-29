4c4155f9d2a3b48d5f3008d0e1e36972c89b7a12
Felix <felixbruell@live.de>
2016-07-29 16:33:35 +0200

    fixed error in Texture.cpp

--- Source/Framework/OpenGL/Texture.cpp
+++ Source/Framework/OpenGL/Texture.cpp
@@ -62,14 +62,14 @@ void Texture::create()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	// use best linear filter
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-
 	// transfer bytes to GPU
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_pRawData.get());
 
 	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// use best linear filter
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void Texture::dispose()
