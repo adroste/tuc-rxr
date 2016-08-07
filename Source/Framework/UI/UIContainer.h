0e798a3020e34271638fd782564ee2ea6b1c6488
Alexander Droste <alexander.droste@progmem.de>
2016-08-07 23:01:21 +0200

    added UIContainer.h and UIContainerDraggable.h

---
+++ Source/Framework/UI/UIContainer.h
@@ -0,0 +1,174 @@
+#pragma once
+#include "UIObject.h"
+#include "../../System/Input.h"
+#include "../../System/Exceptions/ExceptionInvalidOperation.h"
+
+class UIContainer : public UIObject, public Input::IReceiver
+{
+public:
+	UIContainer(bool show = false)
+	{
+		show ? UIContainer::show() : UIContainer::hide();
+	}
+
+	virtual ~UIContainer() override
+	{
+		if (m_parent)
+		{
+			unregisterMe(m_parent);
+			m_parent = nullptr;
+		}
+	}
+
+	virtual void update(float dt)
+	{}
+
+	virtual void draw(Drawing& draw) override
+	{
+		if (!isVisible()) return;
+
+		// TODO draw border, background
+		// just testing
+		{
+			draw.rect(getRect(), Color::Gray());
+			draw.rect(getRect().shrink(2.0f), Color::DarkGray());
+		}
+
+		draw.getTransform().pushModel(m_matTransform);
+		for (auto obj : m_uiObjects)
+		{
+			obj->draw(draw);
+		}
+		draw.getTransform().popModel();
+	}
+
+	// Input
+	virtual bool mouseDown(const PointF& mpos, Input::Mouse button) override
+	{
+		return m_isMouseInside;
+	}
+
+	virtual bool mouseUp(const PointF& mpos, Input::Mouse button) override
+	{
+		return m_isMouseInside;
+	}
+
+	virtual bool mouseMove(const PointF& mpos, bool handled) override
+	{
+		m_isMouseInside = getRect().isPointInside(mpos);
+		return m_isMouseInside;
+	}
+
+	virtual bool wheel(const PointF& mpos, float amount) override
+	{
+		return m_isMouseInside;
+	}
+
+
+	virtual void setOrigin(const PointF& p) override
+	{
+		IMetrics::setOrigin(p);
+		m_matTransform = glm::translate(glm::vec3(p.x, p.y, 0.0f));
+		for (auto r : m_receivers)
+			r->setTransform(getTransform() * glm::translate(glm::vec3(-p.x, -p.y, 0.0f)));
+	}
+
+	virtual void setZIndex(int z) override
+	{
+		int zdiff = z - getZIndex();
+		Input::IReceiver::setZIndex(z);
+
+		for (auto r : m_receivers)
+			r->setZIndex(r->getZIndex() + zdiff);
+	}
+
+	virtual void setTransform(glm::mat4 transform) override
+	{
+		Input::IReceiver::setTransform(transform);
+		for (auto r : m_receivers)
+			r->setTransform(transform * r->getTransform());
+	}
+
+	// register after all receivers have been added
+	virtual void registerMe(GameState* parent) override final
+	{
+		m_parent = parent;
+		Input::IReceiver::registerMe(parent);
+		for (auto r : m_receivers)
+			r->registerMe(parent);
+	}
+
+	virtual void unregisterMe(GameState* parent) override final // do not remove this final!! 
+	{
+		for (auto r : m_receivers)
+			r->unregisterMe(parent);
+		Input::IReceiver::unregisterMe(parent);
+	}
+
+	virtual void enable() override
+	{
+		Input::IReceiver::enable();
+		for (auto r : m_receivers)
+			r->enable();
+	}
+
+	virtual void disable() override
+	{
+		for (auto r : m_receivers)
+			r->disable();
+		Input::IReceiver::disable();
+	}
+
+	/*virtual void setVisibility(bool visible) override
+	{
+	UIObject::setVisibility(visible);
+	for (auto o : m_uiObjects)
+	o->setVisibility(visible);
+	}
+	do not use: ui objects visible status should reveal if objects are visible INSIDE the container, not at all*/
+
+	virtual void hide()
+	{
+		m_isShown = false;
+		disable();
+		setVisibility(false);
+	}
+
+	virtual void show()
+	{
+		m_isShown = true;
+		setVisibility(true);
+		enable();
+	}
+
+	bool isShown() const
+	{
+		return m_isShown;
+	}
+
+protected:
+	// every container object needs to be added with this function
+	virtual void addUIObject(UIObject* obj)
+	{
+		m_uiObjects.push_back(obj);
+		Input::IReceiver* pRec = dynamic_cast<Input::IReceiver*>(obj);
+		if (pRec)
+		{
+			if (pRec->getZIndex() <= getZIndex())
+				throw ExceptionInvalidOperation("UIWindow::addUIObject", "z-index of object is <= z-index of window");
+			m_receivers.push_back(pRec);
+		}
+		m_isShown ? enable() : disable();
+	}
+
+protected:
+	glm::mat4 m_matTransform;
+	bool m_isMouseInside = false;
+
+private:
+	GameState* m_parent = nullptr;
+	std::vector<Input::IReceiver*> m_receivers;
+	std::vector<UIObject*> m_uiObjects;
+
+	bool m_isShown;
+};
--- Source/Framework/UI/UIWindow.h
+++ Source/Framework/UI/UIWindow.h
@@ -1,56 +1,27 @@
 #pragma once
-#include "UIObject.h"
-#include "../../System/Input.h"
-#include "../GameState.h"
+#include "UIContainerDraggable.h"
 
-class UIWindow : public UIObject, public Input::IReceiver
+class UIWindow : public UIContainerDraggable
 {
 public:
 	UIWindow(bool show = false)
 		:
+		UIContainerDraggable(show),
 		m_btnCancel(UIButton::Style::Royal, Drawing::getFont(Font::Style::Headline, Font::Size::S), "X")
 	{
 		m_btnCancel.setZIndex(getZIndex() + 1);
 		m_btnCancel.adjustToFontHeadline();
 		UIWindow::addUIObject(&m_btnCancel);
-
-		show ? UIWindow::show() : UIWindow::hide();
 	}
 
 	virtual ~UIWindow() override
-	{
-		if (m_parent)
-		{
-			unregisterMe(m_parent);
-			m_parent = nullptr;
-		}
-	}
+	{}
 
-	virtual void update(float dt)
+	virtual void update(float dt) override
 	{
 		if (m_btnCancel.isClicked(true))
 			hide();
 	}
-
-	virtual void draw(Drawing& draw) override
-	{
-		if (!isVisible()) return;
-
-		// TODO draw border, background
-		// just testing
-		{
-			draw.rect(getRect(), Color::Gray());
-			draw.rect(getRect().shrink(2.0f), Color::DarkGray());
-		}
-
-		draw.getTransform().pushModel(m_matTransform);
-		for (auto obj : m_uiObjects)
-		{
-			obj->draw(draw);
-		}
-		draw.getTransform().popModel();
-	}
-
 
 	virtual bool keyUp(SDL_Scancode s) override
 	{
@@ -62,158 +33,12 @@ public:
 		return false;
 	}
 
-	virtual bool mouseDown(const PointF& mpos, Input::Mouse button) override
-	{
-		if (m_isMouseInside && button == Input::Mouse::Left)
-		{
-			m_isMouseLeftDown = true;
-			m_dragSpot = mpos;
-		}
-
-		return m_isMouseInside;
-	}
-
-	virtual bool mouseUp(const PointF& mpos, Input::Mouse button) override
-	{
-		if (button == Input::Mouse::Left)
-			m_isMouseLeftDown = false;
-
-		return m_isMouseInside;
-	}
-
-	virtual bool mouseMove(const PointF& mpos, bool handled) override
-	{
-		m_isMouseInside = getRect().isPointInside(mpos);
-
-		// drag window
-		if (m_isMouseLeftDown)
-		{
-			setOrigin(getOrigin() + mpos - m_dragSpot);
-			m_dragSpot = mpos;			
-		}
-
-		return m_isMouseInside;
-	}
-
-	virtual bool wheel(const PointF& mpos, float amount) override
-	{
-		return m_isMouseInside;
-	}
-
-
-	virtual void setOrigin(const PointF& p) override
-	{
-		IMetrics::setOrigin(p);
-		m_matTransform = glm::translate(glm::vec3(p.x, p.y, 0.0f));
-		for (auto r : m_receivers)
-			r->setTransform(getTransform() * glm::translate(glm::vec3(-p.x, -p.y, 0.0f)));
-	}
-
 	virtual void setDim(const PointF& d) override
 	{
 		IMetrics::setDim(d);
 		m_btnCancel.setOrigin({ d.x - m_btnCancel.getDim().x + 30.0f, -20.0f });
 	}
 
-	virtual void setZIndex(int z) override
-	{
-		int zdiff = z - getZIndex();
-		Input::IReceiver::setZIndex(z);
-
-		for (auto r : m_receivers)
-			r->setZIndex(r->getZIndex() + zdiff);
-	}
-
-	virtual void setTransform(glm::mat4 transform) override
-	{
-		Input::IReceiver::setTransform(transform);
-		for (auto r : m_receivers)
-			r->setTransform(transform * r->getTransform());		
-	}
-
-	// register after all UIObjects has been added
-	virtual void registerMe(GameState* parent) override final
-	{
-		m_parent = parent;
-		Input::IReceiver::registerMe(parent);
-		for (auto r : m_receivers)
-			r->registerMe(parent);
-	}
-
-	virtual void unregisterMe(GameState* parent) override final // do not remove this final!! 
-	{
-		for (auto r : m_receivers)
-			r->unregisterMe(parent);
-		Input::IReceiver::unregisterMe(parent);
-	}
-
-	virtual void enable() override
-	{
-		Input::IReceiver::enable();
-		for (auto r : m_receivers)
-			r->enable();
-	}
-
-	virtual void disable() override
-	{
-		for (auto r : m_receivers)
-			r->disable();
-		Input::IReceiver::disable();
-	}
-
-	/*virtual void setVisibility(bool visible) override
-	{
-		UIObject::setVisibility(visible);
-		for (auto o : m_uiObjects)
-			o->setVisibility(visible);
-	}
-	do not use: ui objects visible status should reveal if objects are visible INSIDE the window, not at all*/
-
-	virtual void hide()
-	{
-		m_isShown = false;
-		disable();
-		setVisibility(false);
-	}
-
-	virtual void show()
-	{
-		m_isShown = true;
-		setVisibility(true);
-		enable();
-	}
-
-	bool isShown() const
-	{
-		return m_isShown;
-	}
-
-protected:
-	// every window object needs to be added with this function (inside constructor)
-	virtual void addUIObject(UIObject* obj)
-	{
-		m_uiObjects.push_back(obj);
-		Input::IReceiver* pRec = dynamic_cast<Input::IReceiver*>(obj);
-		if (pRec)
-		{
-			if (pRec->getZIndex() <= getZIndex())
-				throw ExceptionInvalidOperation("UIWindow::addUIObject", "z-index of object is <= z-index of window");
-			m_receivers.push_back(pRec);
-		}
-	}
-
-protected:
-	glm::mat4 m_matTransform;
-	bool m_isMouseInside = false;
-	bool m_isMouseLeftDown = false;
-	PointF m_dragSpot;
-	
+protected:	
 	UIButtonText m_btnCancel;
-
-private:
-	GameState* m_parent = nullptr;
-	std::vector<Input::IReceiver*> m_receivers;
-	std::vector<UIObject*> m_uiObjects;
-
-	bool m_isShown;
 };
