f83ea86178947bdd0e210b4be2fb9a6cd4d8a905
Alexander Droste <alexander.droste@progmem.de>
2016-08-08 09:49:33 +0200

    UIObjects are now controls (implementing IReceiver)

--- Source/Framework/UI/UIObject.h
+++ Source/Framework/UI/UIObject.h
@@ -2,15 +2,45 @@
 #include "../OpenGL/IDrawable.h"
 #include "Interfaces/IMetrics.h"
 
-class UIObject : public IDrawable, public IMetrics
+class UIObject : public IDrawable, public IMetrics, public Input::IReceiver
 {
 public:
+	UIObject()
+		:
+		m_isEnabled(Input::IReceiver::isEnabled())
+	{}
+
 	virtual ~UIObject() override
 	{}
+
+
+	virtual void enable() override
+	{
+		m_isEnabled = true;
+		if (m_isVisible)
+			Input::IReceiver::enable();		
+	}
+
+	virtual void disable() override
+	{
+		m_isEnabled = false;
+		if (m_isVisible)
+			Input::IReceiver::disable();
+	}
+
+	virtual bool isEnabled() const override
+	{
+		return m_isEnabled;
+	}
 
 	virtual void setVisibility(bool visible)
 	{
 		m_isVisible = visible;
+
+		if (m_isVisible)
+			m_isEnabled ? Input::IReceiver::enable() : Input::IReceiver::disable();
+		else
+			Input::IReceiver::disable();
 	}
 
 	bool isVisible() const
@@ -19,5 +49,6 @@ public:
 	}
 
 private:
+	bool m_isEnabled;
 	bool m_isVisible = true;
 };
