0fa0b7e20e334ebe317732ad8006916a70616785
Felix Brüll <felixbruell@live.de>
2016-08-10 22:34:46 +0200

    added UIItemLister which can list UIObjects moved the #include <windows> stuff from system.h to system.cpp to prevent definition of min max macro (because we want to use std::min std::max)

---
+++ Source/Framework/UI/UIItemLister.h
@@ -0,0 +1,112 @@
+#pragma once
+#include "UIContainer.h"
+#include "../../Utility/Mutex.h"
+
+/*
+	This Container can order Items like the following:
+
+		ItemA	ItemB
+		Test	Test2
+		Caption
+		Left	Right
+
+	How to use
+		1. add all items to lister
+		2. call registerMe
+		3. call orderItems (metrics of all objects MUST be valid)
+*/
+class UIItemLister : public UIContainer
+{
+	struct Item
+	{
+		UIObject* left = nullptr;
+		UIObject* right = nullptr;
+	};
+public:
+	UIItemLister()
+		:
+		UIContainer(true)
+	{}
+	virtual ~UIItemLister()
+	{}
+	// arguments may be nullptr
+	void addToList(UIObject* left, UIObject* right)
+	{
+		Item i;
+		i.left = left;
+		i.right = right;
+
+		LockGuard g(m_muItms);
+		m_items.push_back(i);
+		g.unlock();
+
+		if (left)
+			left->registerMe(this);
+		if (right)
+			right->registerMe(this);
+	}
+	void orderItems()
+	{
+		float curY = m_pos.y + PADDING;
+
+		// TODO adjustable width
+		float widthLeft = m_dim.x / 2.0f;
+		float widthRight = widthLeft;
+
+		float leftStart = m_pos.x + PADDING;
+		float leftEnd = m_pos.x + widthLeft - PADDING;
+		float rightStart = m_pos.x + widthLeft + PADDING;
+		float rightEnd = m_pos.x + widthLeft + widthRight - PADDING;
+
+		for(auto& i : m_items)
+		{
+			float maxHei = 0.0f;
+			if (i.left)
+				maxHei = i.left->getDim().y;
+			if (i.right)
+				maxHei = std::max(maxHei, i.right->getDim().y);
+
+			if (i.left)
+				clipToRect(i.left, RectF(leftStart, curY, leftEnd, curY + maxHei));
+
+			if (i.right)
+				clipToRect(i.left, RectF(rightStart, curY, rightEnd, curY + maxHei));
+
+			curY += maxHei;
+		}
+
+		sortReceivers();
+	}
+
+	virtual void draw(Drawing& draw) override
+	{
+		if(isVisible())
+		{
+			UIContainer::draw(draw);
+
+			LockGuard g(m_muItms);
+			for (auto& i : m_items)
+			{
+				if (i.left)
+					i.left->draw(draw);
+
+				if (i.right)
+					i.right->draw(draw);
+			}
+		}
+	}
+private:
+	void clipToRect(UIObject* o, const RectF& r)
+	{
+		float y = (r.y1 + r.y2) / 2.0f - o->getDim().y / 2.0f;
+		float x = (r.x1 + r.x2) / 2.0f - o->getDim().x / 2.0f;
+
+		o->setOrigin({ x,y });
+	}
+
+protected:
+	std::vector<Item> m_items;
+	Mutex m_muItms;
+
+	const float PADDING = 5.0f;
+};
--- Source/System/System.h
+++ Source/System/System.h
@@ -6,11 +6,6 @@
 #include "Log.h"
 #include "../SDL/SDL.h"
 
-#if _WIN32
-#define _WINSOCKAPI_
-#include <windows.h>
-#endif
-
 class System
 {
 public:
@@ -53,46 +48,7 @@ public:
 	{
 		return SDL_IsScreenSaverEnabled() != SDL_FALSE;
 	}
-	static void messageBox(const std::string& title, const std::string& message, BoxIcon icon)
-	{
-#if _WIN32
-		UINT cicon = 0;
-		switch(icon)
-		{
-		case BoxIcon::Warning:
-			cicon = MB_ICONWARNING;
-			break;
-		case BoxIcon::Info:
-			cicon = MB_ICONINFORMATION;
-			break;
-		case BoxIcon::Error:
-			cicon = MB_ICONERROR;
-			break;
-		default: break;
-		}
-		MessageBoxA(nullptr, message.c_str(), title.c_str(), MB_OK | cicon);
-#else
-		SDL_MessageBoxData dat;
-		dat.flags = static_cast<Uint32>(icon);
-		dat.window = nullptr;
-		dat.title = title.c_str();
-		dat.message = message.c_str();
-
-		// buttons
-		SDL_MessageBoxButtonData btn;
-		btn.buttonid = 0;
-		btn.flags = SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT;
-		btn.text = "ok";
-
-		dat.numbuttons = 1;
-		dat.buttons = &btn;
-
-		dat.colorScheme = nullptr;
-
-		int pressed;
-		SDL_ShowMessageBox(&dat, &pressed);
-#endif
-	}
+	static void messageBox(const std::string& title, const std::string& message, BoxIcon icon);
 	static void sleep(Uint32 ms)
 	{
 		SDL_Delay(ms);
