fe181f20995dacce664491a5c82c0647a94e2aed
Felix <felixbruell@live.de>
2016-11-06 12:54:21 +0100

    fixed asset displacement + added rotation to assets

--- Source/Game/Client/MapAsset.cpp
+++ Source/Game/Client/MapAsset.cpp
@@ -11,13 +11,14 @@ void MapAsset::loadChunk(const std::vector<std::pair<Point3S, CubeDesc>>& cubes)
 
 void MapAsset::addInstance(glm::vec3 pos, float theta, float phi, float scale)
 {
-	// TODO add rotation
-	// TODO fix displacement
+	auto phiRot = glm::rotate(phi, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::vec4 z = phiRot * glm::vec4(0.0f, 0.0f, 1.0f, 0.0f);
 	m_instRot.push_back(
 		
-		glm::translate(pos + glm::vec3(0.5f)/* + glm::vec3(0.0f,0.55f,0.0f)*/) * glm::scale(glm::vec3(scale)) *
+		glm::translate(pos - glm::vec3(0.0f,0.5f * (1.0f - scale),0.0f)) * glm::scale(glm::vec3(scale)) *
+		glm::rotate(theta,glm::vec3(z.x,z.y,z.z)) * phiRot *
 		// translate to origin
-		glm::translate(glm::vec3(-float(MapChunk::SIZE / 2), -float(MapChunk::SIZE), -float(MapChunk::SIZE / 2)))
+		glm::translate(glm::vec3(-float(MapChunk::SIZE / 2) + 0.5f, -float(MapChunk::SIZE), -float(MapChunk::SIZE / 2) + 0.5f))
 
 	);
 }
