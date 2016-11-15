5c7bf66917c4062eb6e266012bae4c2db8ba20d5
Felix <felixbruell@live.de>
2016-11-15 13:34:10 +0100

    added Disposable for gl objects to solve the gpu memory leak problem

--- Source/Framework/OpenGL/glhelper.h
+++ Source/Framework/OpenGL/glhelper.h
@@ -1,11 +1,56 @@
 #pragma once
 #include "../../glew/glew.h"
 #include <algorithm>
+#include <cassert>
 
 namespace gl
 {
 	using deleter = void(*)(GLsizei, GLuint*);
 	using generator = void(*)(GLsizei, GLuint*);
+
+	template <generator, deleter>
+	class Data;
+	class Disposeable
+	{
+		template <generator, deleter>
+		friend Data<generator,deleter>;
+
+		Disposeable(GLuint id, deleter del)
+			:
+		m_deleter(del),m_id(id)
+		{}
+	public:
+		Disposeable()
+		{}
+		Disposeable(const Disposeable&) = delete;
+		Disposeable& operator=(const Disposeable&) = delete;
+		Disposeable(Disposeable&& m) noexcept
+		{
+			swap(m);
+		}
+		Disposeable& operator=(Disposeable&& m) noexcept
+		{
+			swap(m);
+			return *this;
+		}
+		~Disposeable()
+		{
+			if(m_id)
+			{
+				assert(m_deleter);
+				m_deleter(1, &m_id);
+				m_id = 0;
+			}
+		}
+		void swap(Disposeable& o) noexcept
+		{
+			std::swap(m_deleter, o.m_deleter);
+			std::swap(m_id, o.m_id);
+		}
+	private:
+		deleter m_deleter = nullptr;
+		GLuint m_id = 0;
+	};
 
 	template <generator gen, deleter del>
 	class Data
@@ -16,12 +61,12 @@ namespace gl
 			gen(1, &m_id);
 		}
 		Data(const Data&) = delete;
-		Data(Data&& o)
+		Data(Data&& o) noexcept
 		{
 			swap(o);
 		}
 		Data& operator=(const Data&) = delete;
-		Data& operator=(Data&& o)
+		Data& operator=(Data&& o) noexcept
 		{
 			swap(o);
 			return *this;
@@ -38,13 +83,19 @@ namespace gl
 		{
 			return m_id;
 		}
-		void swap(Data<gen,del>& o)
+		void swap(Data<gen,del>& o) noexcept
 		{
 			std::swap(m_id, o.m_id);
 		}
 		static deleter getDeleter()
 		{
 			return del;
+		}
+		Disposeable dispose()
+		{
+			Disposeable d(m_id, del);
+			m_id = 0;
+			return d;
 		}
 	private:
 		GLuint m_id = 0;
