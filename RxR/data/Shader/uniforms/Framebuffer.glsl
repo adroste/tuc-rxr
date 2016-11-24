8ac97dfda4ab839d462e8e07206404c6e413c39b
Felix Brüll <felixbruell@live.de>
2016-11-24 21:06:10 +0100

    updated uniforms/Framebuffer.glsl for alex

--- RxR/data/Shader/uniforms/Framebuffer.glsl
+++ RxR/data/Shader/uniforms/Framebuffer.glsl
@@ -2,4 +2,5 @@
 layout( std140 ) uniform Framebuffer
 {
 	vec2 pstep;
+	vec2 dummy; // if i dont add this alex's graphics card will optimize it away -> resulting in a deiferent uniform buffer size...
 } frame;
