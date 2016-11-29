3b16be4ef432b9107be438ad7b5b3638566ac262
Felix <felixbruell@live.de>
2016-11-29 14:39:00 +0100

    optimized vertex shader

--- RxR/data/Shader/cube/vertex.glsl
+++ RxR/data/Shader/cube/vertex.glsl
@@ -24,39 +24,36 @@ layout(location = 9) flat out float out_glowing;
 
 void main()
 {
-	uint neighbors  = (uint(in_iinfo.z) & uint(0xFC000000)) >> 26;
-	out_neighbors = neighbors;
-	uint side = uint(0);
+	out_neighbors  = (uint(in_iinfo.z) & uint(0xFC000000)) >> 26;
+	out_cubeSide = uint(0);
 	
-	vec3 normal = in_normal;
-	vec3 pos = in_position;
+	out_normal = in_normal;
 	
-	if(normal.x != 0.0)
+	if(out_normal.x != 0.0)
 	{
-		if(normal.x > 0.0) // right
-			side = uint(2);
+		if(out_normal.x > 0.0) // right
+			out_cubeSide = uint(2);
 		else
-			side = uint(1);
+			out_cubeSide = uint(1);
 	}
-	else if(normal.y != 0.0)
+	else if(out_normal.y != 0.0)
 	{
-		if(normal.y > 0.0) // top
-			side = uint(4);
+		if(out_normal.y > 0.0) // top
+			out_cubeSide = uint(4);
 		else
-			side = uint(8);
+			out_cubeSide = uint(8);
 	}
 	else // z != 0
 	{
-		if(normal.z > 0.0) // front
-			side = uint(16);
+		if(out_normal.z > 0.0) // front
+			out_cubeSide = uint(16);
 		else
-			side = uint(32);
+			out_cubeSide = uint(32);
 	}
-	out_cubeSide = side;
-	uint plsDiscard = neighbors & side;
-	if(plsDiscard != uint(0))
+	
+	if((out_neighbors & out_cubeSide) != uint(0))
 	{
-		// move outside window
+		// move out of window
 		gl_Position = vec4(-2.0,-2.0,0.0,1.0);
 		return;
 	}
@@ -85,21 +82,19 @@ void main()
 	out_specColor = vec4(float(r) / 255.0, float(g) / 255.0, float(b) / 255.0, float(igloss));
 	
 	out_shaderType = (uint(in_iinfo.z) & uint(0x070000)) >> 16;
-	uint glow = (uint(in_iinfo.z) & uint(0x080000)) >> 19;
-	out_glowing = float(glow);
+	out_glowing = float((uint(in_iinfo.z) & uint(0x080000)) >> 19);
 	
-	vec3 inChunkPos = pos * 0.5 + chOffset;
-	vec3 mapPos;
+	vec3 inChunkPos = in_position * 0.5 + chOffset;
 	
 	if(animation == uint(0))
 	{
-		out_normal = (matModel * vec4(normal,0.0)).xyz;
-		mapPos = (matModel * vec4(inChunkPos, 1.0)).xyz;
+		out_normal = (matModel * vec4(out_normal,0.0)).xyz;
+		out_mapPos = (matModel * vec4(inChunkPos, 1.0)).xyz;
 		gl_Position = matProjection * matCamera * matModel * vec4(inChunkPos, 1.0);
 	}
 	else // wind animation
 	{
-		// draw cube from back to front -> flip sides
+		// draw cube from back to front -> flip out_cubeSides
 		float xfactor = sin(framework.random.z * 6.28318531) * 7.0;
 		float zfactor = cos(framework.random.y * 6.28318531) * 7.0;
 		float h = 1.0 - (inChunkPos.y + 0.5) / float(CHUNK_SIZE); // [0-1]
@@ -107,23 +102,23 @@ void main()
 		inChunkPos.x = inChunkPos.x + h * xfactor;
 		inChunkPos.z = inChunkPos.z + h * zfactor;
 		
-		out_normal = (matModel * vec4(normal,0.0)).xyz;
-		mapPos = (matModel * vec4(inChunkPos, 1.0)).xyz;
+		out_normal = (matModel * vec4(out_normal,0.0)).xyz;
+		out_mapPos = (matModel * vec4(inChunkPos, 1.0)).xyz;
 		gl_Position = matProjection * matCamera * matModel * vec4(inChunkPos, 1.0);
 	}
-	out_mapPos = mapPos;
 	
-	switch(side)
+	// flow animation
+	switch(out_cubeSide)
 	{
 	case uint(4):
 	case uint(8): // top + bottom
-		out_texCoord0 = mapPos.xz / vec2(16.0) + framework.random.xy;
+		out_texCoord0 = out_mapPos.xz / vec2(16.0) + framework.random.xy;
 		break;
 	case uint(1): // left rigt
 	case uint(2):
-		out_texCoord0 = (mapPos.zy + vec2(8.0)) / vec2(16.0) + framework.random.xy;
+		out_texCoord0 = (out_mapPos.zy + vec2(8.0)) / vec2(16.0) + framework.random.xy;
 	default: // front back
-		out_texCoord0 = mapPos.xy / vec2(16.0) - vec2(0.0,framework.random.w);
+		out_texCoord0 = out_mapPos.xy / vec2(16.0) - vec2(0.0,framework.random.w);
 		
 	}
 }
